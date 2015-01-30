38e43a86734e4302056ded907d75e19e5fed181d
Gilbert Wong <gilbertwong@maaii.com>
2015-01-30 19:11:45 +0800

    Split email sending logic of mailer
    Mailer is only a wrapper around the SMTP transport. The transport itself
    already can send email with its own "#sendMail". What mailer adds is
    just that it accepts an extra parameter for the mail content (instead
    of adding it to 'e-mail message field').
    
    'templateMailer' is a variation of mailer that has template-related
    features.
    
    To trigger the 'test':
    `recipient='user@maaii.com' mocha --compilers coffee:coffee-script/register test/unit/app/lib/mailer/index.coffee`
    
    See:
    - https://github.com/andris9/Nodemailer#e-mail-message-fields
    - https://github.com/andris9/Nodemailer#use-the-default-smtp-transport
    
    Change-Id: I6c325d2f48a948bb4897ea1e07ea9bef0e213424

---
+++ app/lib/mailer/templateMailer.ts
@@ -0,0 +1,69 @@
+/// <reference path='../../../typings/node/node.d.ts' />
+
+var emailTemplates = require('email-templates');
+var fs             = require('fs');
+var logger         = require('winston');
+
+var TemplateMailer = module.exports = function(mailer, opts) {
+  if(!mailer) throw new Error('mailer is required');
+  this.mailer = mailer;
+
+  this._templatesDir = opts.templatesDir;
+  if(!fs.existsSync(this._templatesDir))
+    throw new Error('templatesDir "' + this._templatesDir + '" doesn\'t not exist');
+}
+
+/**
+ * Send email using the data with the template specified
+ *
+ * @param {Object}
+ * @param {String}
+ * @param {Object}
+ * @param {Function} cb
+ */
+TemplateMailer.prototype.send = function(mailOpts, tmplName, tmplData, cb) {
+  //TODO validate mailOpts
+  if(!mailOpts || !tmplName || !tmplData) throw new Error('Invalid number of arguments');
+
+  this._tmplContent(tmplName, tmplData, function(err, html) { /* text */
+    if(err) return cb(err);
+
+    mailOpts.html = html;
+
+    // TODO change it to make use of mailer
+    this.mailer.sendHtmlContent(mailOpts, html, function(err, responseStatus) {
+      if (err) return cb(err);
+
+      logger.info('Sending email using template %s with %j', tmplName, tmplData, {});
+      cb(null, responseStatus.message);
+    });
+  }.bind(this));
+}
+
+/**
+ * Prepare the message content with the specified template & data
+ *
+ * @private
+ * @param {String} tmplName
+ * @param {Object} tmplData
+ * @param {Function} cb
+ */
+TemplateMailer.prototype._tmplContent = function(tmplName, tmplData, cb) {
+  emailTemplates(this._templatesDir, function(err, template) {
+    if (err) {
+      logger.error('Error lookup templates directory, %s', this._templatesDir, err.stack);
+      return cb(err);
+    }
+
+    template(tmplName, tmplData, function(err, html, text) {
+      if(err) {
+        logger.error('Error prepare template %s with %j', tmplName, tmplData, err.stack, {});
+        return cb(err);
+      }
+      cb(null, html, text);
+    });
+  });
+};
+
+
+
