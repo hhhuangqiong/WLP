280bc9ed31b20958a98e15e2333d81e8cd575247
kareemelshahawe <kareemelshahawe@maaii.com>
2015-01-30 15:14:09 +0800

    Add root company seed to the dataseed.
    NB: rootCompany data is stored in the dataseed.json as rootCompany.
    
    Change-Id: Id21fa00edeeed090567ccad01ca98e86ec367cfc

--- app/server/initializers/dataseed.ts
+++ app/server/initializers/dataseed.ts
@@ -1,14 +1,19 @@
 /// <reference path='../../../typings/winston/winston.d.ts' />
 import logger   = require('winston');
 
-var fs         = require('fs');
+var fs = require('fs');
 var PortalUser = require('app/collections/portalUser');
-
-function initialize(seedFilePath: string) {
-  var content  = JSON.parse(fs.readFileSync(seedFilePath, {encoding: 'utf8'}));
+var companyModel = require('app/collections/company');
+function initialize(seedFilePath:string) {
+  var content = JSON.parse(fs.readFileSync(seedFilePath, {encoding: 'utf8'}));
 
   // assume there can only have 1 and only 1 root user
   PortalUser.findOneAndUpdate({isRoot: true}, content.rootUser, {upsert: true}, function (err, user) {
+    if (err) throw err;
+  });
+
+  //assume there can only have 1 and only 1 root company
+  companyModel.findOneAndUpdate(content.rootCompany, {upsert: true}, function (err, company) {
     if (err) throw err;
   });
 }
