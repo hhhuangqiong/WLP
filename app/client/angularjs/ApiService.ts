04c301528f389a9c48fdf60e5390751e15fbd3f3
kareemelshahawe <kareemelshahawe@maaii.com>
2015-02-03 17:05:12 +0800

    Add delete to ApiService.
    - Modify ApiService to move repeated implementation to execute function.
    
    Change-Id: I649bd16495e0ea288a6b085e6dad70cc0e56dd25

--- app/client/angularjs/ApiService.ts
+++ app/client/angularjs/ApiService.ts
@@ -4,12 +4,12 @@ module whitelabel {
 
   export class ApiService {
 
-    apiUrl: string;
-    $http: ng.IHttpService;
-    $q: ng.IQService;
-    $log: ng.ILogService;
+    apiUrl:string;
+    $http:ng.IHttpService;
+    $q:ng.IQService;
+    $log:ng.ILogService;
 
-    constructor(apiUrl: string, $http:ng.IHttpService, $q:ng.IQService, $log:ng.ILogService) {
+    constructor(apiUrl:string, $http:ng.IHttpService, $q:ng.IQService, $log:ng.ILogService) {
       this.apiUrl = apiUrl;
       this.$http = $http;
       this.$q = $q;
@@ -20,42 +20,42 @@ module whitelabel {
       var deferred = this.$q.defer();
 
       this.$http.get(this.composeApiUrl(resource, id))
-        .success(function(data: any, status, headers, config) {
+        .success(function (data:any, status, headers, config) {
           deferred.resolve(data[resource]);
         })
-        .error(function(error) {
+        .error(function (error) {
           deferred.reject(error);
         });
 
       return deferred.promise;
     };
 
-    post = (resource:string, objectParams: any) => {
-      var deferred = this.$q.defer();
 
-      this.$http.post(this.composeApiUrl(resource), objectParams)
-        .success(function(data: any, status: any, header: any, config: any) {
+    post = (resource:string, objectParams:any) => {
+      return this.execute('post', resource, objectParams);
+    };
+
+    put = (resource:string, objectParams:any) => {
+      return this.execute('put', resource, objectParams);
+    };
+
+    delete = (resource:string, objectParams:any) => {
+      return this.execute('delete', resource, objectParams);
+    };
+
+    execute = (method, resource, objectParams?)=> {
+      var deferred = this.$q.defer();
+      this.$http[method](this.composeApiUrl(resource), objectParams)
+        .success(function (data:any, status:any, header:any, config:any) {
           deferred.resolve(data.user);
         })
-        .error(function(error: any) {
+        .error(function (error:any) {
           deferred.reject(error);
         });
 
       return deferred.promise;
-    };
 
-    put = (resource:string, objectParams: any) => {
-      var deferred = this.$q.defer();
-      this.$http.put(this.composeApiUrl(resource), objectParams)
-        .success(function(data: any, status: any, header: any, config: any) {
-          deferred.resolve(data.user);
-        })
-        .error(function(error: any) {
-          deferred.reject(error);
-        });
-
-      return deferred.promise;
-    };
+    }
 
     /**
      * Making up Api Url
@@ -64,7 +64,7 @@ module whitelabel {
      * @returns {string}
      */
 
-    composeApiUrl(resource: string, id?: string) {
+    composeApiUrl(resource:string, id?:string) {
       var _url = this.apiUrl + '/' + resource;
       if (id) {
         _url += '/' + id;
@@ -74,7 +74,7 @@ module whitelabel {
     }
   }
 
-  app.factory('ApiService', function($http: ng.IHttpService, $q: ng.IQService, $log: ng.ILogService) {
+  app.factory('ApiService', function ($http:ng.IHttpService, $q:ng.IQService, $log:ng.ILogService) {
     return new ApiService('/api', $http, $q, $log);
   })
 }
