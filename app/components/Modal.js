22f8676a2288af3ab4f13d73c54621f526df333c
Gilbert Wong <gilbertwong@maaii.com>
2015-05-18 13:05:22 +0800

    Normalize the case for file names
    Change-Id: Ie027fdca66e914f450c4116d86d3b542a5ff9ef8

--- app/components/Modal.js
+++ app/components/Modal.js
@@ -1,26 +1,58 @@
 'use strict';
 import React from 'react';
+import ModalStore from '../stores/ModalStore';
+import FluxibleMixin from 'fluxible/addons/FluxibleMixin';
 
 var modal = React.createClass({
-  displayName: 'pop up',
+  mixins: [FluxibleMixin],
+  statics: {
+    storeListeners: [ModalStore]
+  },
+  /**
+   * get states of Company List and Current Company from CompanyStore
+   *
+   * @returns {Object}
+   */
+  getInitialState: function () {
+    return this.getStore(ModalStore).getState();
+  },
+  /**
+   * capture Company Store changes and take effect
+   */
+  handleChange:function(){
+    //rendering modal content
+    var contentDiv = $('<div>');
+    $("#myModal").empty(); // clear element each updated;
+    var reveal = $("#myModal").append("<p class='text-center'>"+this.state.Title+"</p><hr/>").append($(contentDiv)).addClass("tiny");
+    $(reveal).foundation('reveal', 'close');
+    $(reveal).bind('closed.fndtn.reveal', function(e){ React.unmountComponentAtNode(this); });
+
+    if(React.isValidElement(this.state.Content)) {
+      React.render(this.state.Content, $(contentDiv)[0]);
+    }
+    else {
+      $(contentDiv).append(this.state.Content);
+    }
+    var condition = (!this.state.isModalOpen) ? "open" : "close";
+    this.setState({isModalOpen: !this.state.isModalOpen})
+    $("#myModal").foundation('reveal', condition);
+  },
+  onChange: function() {
+    let state = this.getStore(ModalStore).getState();
+    this.setState(state);
+    this.handleChange();
+  },
   render: function(){
     return (
       <div id="myModal" className="reveal-modal" data-reveal aria-labelledby="modalTitle" aria-hidden="true" role="dialog">
       </div>
     );
   },
-  componentDidMount: function(){
-    var contentDiv = $('<div>');
-    var reveal = $("#myModal").append("<p class='text-center'>"+this.props.modalTitle+"</p><hr/>").append($(contentDiv)).addClass("tiny");
-    $(reveal).foundation().foundation('reveal', 'close');
-    $(reveal).bind('closed.fndtn.reveal', function(e){ React.unmountComponentAtNode(this); });
-
-    if(React.isValidElement(this.props.children)) {
-      React.render(this.props.children, $(contentDiv)[0]);
-    }
-    else {
-      $(contentDiv).append(this.props.children);
-    }
+  componentDidMount: function () {
+    var elm = this;
+    $(document).on('closed.fndtn.reveal', '[data-reveal]', function () {
+      elm.setState({isModalOpen: false});
+    });
   }
 });
 export default modal;
