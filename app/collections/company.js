79cea6e282660f4728d85fd4a3209d18a04f462a
Gilbert Wong <gilbertwong@maaii.com>
2015-05-18 18:45:11 +0800

    Fix root user seeding logic
    Should set 'affiliatedCompany' during seeding
    
    Change-Id: I4f5df782b82efa8cf545307405e7e88a1f1f792d

--- app/collections/company.js
+++ app/collections/company.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var mongoose = require('mongoose');
 var collectionName = 'Company';
 var schema = new mongoose.Schema({
--- app/server/initializers/dataseed.js
+++ app/server/initializers/dataseed.js
@@ -7,38 +7,58 @@ var fs         = require('fs');
 var Company    = require('../../collections/company');
 var PortalUser = require('../../collections/portalUser');
 
+/**
+ * initialize 'super user' information
+ *
+ * @param {string} seedFilePath path to the seed file
+ */
 function initialize(seedFilePath) {
+  var content;
+  try {
+    content = JSON.parse(fs.readFileSync(seedFilePath, { encoding: 'utf8' }));
+  } catch (e) {
+    throw new Error('Error parsing data seed file');
+  }
+
   // assume there can only have 1 and only 1 root user
   PortalUser.findOne({ isRoot: true }, function (err, user) {
-    if (err) {
-      throw err;
+    if (err) { throw err; }
+
+    if(user)  {
+      logger.info('`root` user already exists; not seeding');
+      return;
     }
 
-    if (!user) {
-        // read file content; blocking
-        var content = JSON.parse(fs.readFileSync(seedFilePath, { encoding: 'utf8' }));
-        var rootUser = content.rootUser;
-        var hashInfo = Q.nbind(PortalUser.hashInfo, PortalUser);
-        var seedUser = function (hashResult) {
-            return Q.ninvoke(PortalUser, 'create', _.merge(rootUser, hashResult));
-        };
-        var seedCompany = function () {
-          var companyInfo = content.rootCompany;
-          var criteria = { name: companyInfo.name };
-          return Q.ninvoke(Company, 'findOneAndUpdate', criteria, companyInfo, { upsert: true });
-        };
-        var infoLogger = function (model) {
-          logger.info('Seeded: %j', model, {});
-        };
-        hashInfo(rootUser.password)
-          .then(seedUser)
-          .then(infoLogger)
-          .then(seedCompany)
-          .then(infoLogger)
-          .catch(function (error) {
-            logger.error('Error during data seeding', error.stack);
-        });
-    }
+    var affiliatedCompanyId;
+    var rootUser = content.user;
+    var hashInfo = Q.nbind(PortalUser.hashInfo, PortalUser);
+    var seedUser = function (hashResult) {
+        var extra = { affiliatedCompany: affiliatedCompanyId };
+        return Q.ninvoke(PortalUser, 'create', _.merge(rootUser, hashResult, extra));
+    };
+    var seedCompany = function (companyInfo) {
+      var condition = { name: companyInfo.name };
+      var opts      = { new: true, upsert: true };
+      return Q.ninvoke(Company, 'findOneAndUpdate', condition, companyInfo, opts);
+    };
+    var infoLogger = function (model) {
+      logger.info('Seeded: %j', model, {});
+    };
+    var putAffiliateIdInScope = function(company) {
+      affiliatedCompanyId = company.id;
+      return company;
+    };
+
+    seedCompany(content.company)
+      .then(putAffiliateIdInScope)
+      .then(infoLogger)
+      .then(hashInfo(rootUser.password))
+      .then(seedUser)
+      .then(infoLogger)
+      .catch(function (error) {
+        logger.error('Error during data seeding', error.stack);
+    });
   });
 }
+
 module.exports = initialize;
