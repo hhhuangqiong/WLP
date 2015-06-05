1672c901382b44848c8621043bac3cc787134441
Marston Ng <marstonng@maaii.com>
2015-06-05 16:12:50 +0800

    Added SignIn validation
    Jira: WLP-38
    Change-Id: I4f9bf6c2ca73766b3fa0788bd3ee389df391e826

--- app/components/SignIn.js
+++ app/components/SignIn.js
@@ -3,9 +3,14 @@ import debug from 'debug';
 import React from 'react';
 import FluxibleMixin from 'fluxible/addons/FluxibleMixin';
 import {Link} from 'react-router'
+import ValidationMixin from 'react-validation-mixin';
+import Joi from 'joi';
+import classNames from 'classnames';
 
 import AuthStore from '../stores/AuthStore';
 import SignInStore from '../stores/SignInStore';
+
+import Tooltip from 'rc-tooltip'
 
 import signInAction from '../actions/signIn';
 
@@ -14,14 +19,22 @@ import PublicOnlyMixin from '../utils/PublicOnlyMixin';
 const bootstrapDebug = debug('app:components:signin');
 
 let SignIn = React.createClass({
-  mixins: [FluxibleMixin, PublicOnlyMixin],
+  mixins: [FluxibleMixin, PublicOnlyMixin, ValidationMixin, React.addons.LinkedStateMixin],
 
   statics: {
     storeListeners: [AuthStore, SignInStore]
   },
 
+  validatorTypes:  {
+    username: Joi.string().email().required().label('Username'),
+    password: Joi.string().min(8).max(30).required().label('Password')
+  },
+
   getInitialState: function() {
-    return this.getStateFromStores();
+    return _.merge({
+      username: null,
+      password: null
+    }, this.getStateFromStores());
   },
 
   getStateFromStores: function () {
@@ -39,15 +52,47 @@ let SignIn = React.createClass({
   handleSignIn(e){
     e.preventDefault();
 
-    let username = this.refs.username.getDOMNode().value.trim();
-    let password = this.refs.password.getDOMNode().value.trim();
-    let rememberMe = this.refs.rememberMe.getDOMNode().checked;
+    this.validate((error, data)=>{
+      if (data.username.length > 0) {
+        React.findDOMNode(this.refs.username).focus();
+        return;
+      }
 
-    this.context.executeAction(signInAction, {
-      username: username,
-      password: password,
-      rememberMe: rememberMe
+      if (data.password.length > 0) {
+        React.findDOMNode(this.refs.password).focus();
+        return;
+      }
+
+      if (!error) {
+        let username = this.refs.username.getDOMNode().value.trim();
+        let password = this.refs.password.getDOMNode().value.trim();
+        //let rememberMe = this.refs.rememberMe.getDOMNode().checked;
+
+        this.context.executeAction(signInAction, {
+          username: username,
+          password: password
+          //rememberMe: rememberMe
+        });
+      }
     });
+  },
+
+  handleInputChange: function(inputName, e) {
+    this.setState({
+      [inputName]: e.target.value.trim()
+    });
+  },
+
+  handleInputBlur: function(inputName) {
+    this.validate(inputName, function(error, validationErrors) {});
+  },
+
+  renderHelpText: function(message) {
+    return (
+      <Tooltip overlay={message}>
+        <a href="#" className="field-set--indicator"><i className="icon-error6" /></a>
+      </Tooltip>
+    );
   },
 
   render() {
@@ -62,12 +107,34 @@ let SignIn = React.createClass({
           <div className="large-offset-1 large-22 columns">
             <div className="row">
               <div className="large-24 columns">
-                <input ref="username" className="radius" type="text" name="username" placeholder="email" />
+                <div className="field-set--validation">
+                  <input
+                    ref="username"
+                    className="radius"
+                    type="text"
+                    name="username"
+                    placeholder="email"
+                    onChange={_.bindKey(this, 'handleInputChange', 'username')}
+                    onBlur={_.bindKey(this, 'handleInputBlur', 'username')}
+                  />
+                  {this.getValidationMessages('username').map(this.renderHelpText)}
+                </div>
               </div>
             </div>
             <div className="row">
               <div className="large-24 columns">
-                <input ref="password" className="radius" type="password" name="password" placeholder="password" />
+                <div className="field-set--validation">
+                  <input
+                    ref="password"
+                    className="radius"
+                    type="password"
+                    name="password"
+                    placeholder="password"
+                    onChange={_.bindKey(this, 'handleInputChange', 'password')}
+                    onBlur={_.bindKey(this, 'handleInputBlur', 'password')}
+                  />
+                  {this.getValidationMessages('password').map(this.renderHelpText)}
+                </div>
               </div>
             </div>
             <div className="row">
