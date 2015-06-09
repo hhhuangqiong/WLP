ebd428859792d724005283443bd5843a1852c378
Andy Hui <andyhui@maaii.com>
2015-06-09 17:41:46 +0800

    Added bolt testing data into dataseed
    Change-Id: I6241c5f34a9762acab78cc33f003951d3f771f1b

--- app/server/initializers/dataseed.js
+++ app/server/initializers/dataseed.js
@@ -1,10 +1,10 @@
 'use strict';
-var logger     = require('winston');
-var Q          = require('q');
-var _          = require('lodash');
-var fs         = require('fs');
+var logger = require('winston');
+var Q = require('q');
+var _ = require('lodash');
+var fs = require('fs');
 
-var Company    = require('../../collections/company');
+var Company = require('../../collections/company');
 var PortalUser = require('../../collections/portalUser');
 
 /**
@@ -15,46 +15,74 @@ var PortalUser = require('../../collections/portalUser');
 function initialize(seedFilePath) {
   var content;
   try {
-    content = JSON.parse(fs.readFileSync(seedFilePath, { encoding: 'utf8' }));
+    content = JSON.parse(fs.readFileSync(seedFilePath, {
+      encoding: 'utf8'
+    }));
   } catch (e) {
     throw new Error('Error parsing data seed file');
   }
 
-  // assume there can only have 1 and only 1 root user
-  PortalUser.findOne({ isRoot: true }, function (err, user) {
-    if (err) { throw err; }
-
-    if(user)  {
-      logger.info('`root` user already exists; not seeding');
-      return;
-    }
+  var insertData = function(data) {
 
     var affiliatedCompanyId;
-    var rootUser = content.user;
+    var rootUser = data.user;
     var hashInfo = Q.nbind(PortalUser.hashInfo, PortalUser);
-    var seedUser = function (hashResult) {
-        var extra = { affiliatedCompany: affiliatedCompanyId };
+    var seedUser = function() {
+
+      return hashInfo(rootUser.password).then(function(hashResult) {
+        var extra = {
+          affiliatedCompany: affiliatedCompanyId
+        };
+        logger.info("Seeding user " +  rootUser.username);
         return Q.ninvoke(PortalUser, 'create', _.merge(rootUser, hashResult, extra));
+      });
+      
     };
-    var seedCompany = function (companyInfo) {
-      var condition = { name: companyInfo.name };
-      var opts      = { new: true, upsert: true };
+    var seedCompany = function(companyInfo) {
+      var condition = {
+        name: companyInfo.name
+      };
+      var opts = {
+        new: true,
+        upsert: true
+      };
       return Q.ninvoke(Company, 'findOneAndUpdate', condition, companyInfo, opts);
     };
-    var infoLogger = function (model) {
+    var infoLogger = function(model) {
       logger.info('Seeded: %j', model, {});
     };
     var putAffiliateIdInScope = function(company) {
       affiliatedCompanyId = company.id;
+      logger.info("Putted affiliatedCompany in scope "+ affiliatedCompanyId);
       return company;
     };
 
-    seedCompany(content.company)
+    seedCompany(data.company)
       .then(putAffiliateIdInScope)
       .then(infoLogger)
-      .then(hashInfo(rootUser.password).then(seedUser).then(infoLogger))
-      .catch(function (error) {
+      .then(seedUser)
+      .then(infoLogger)
+      .catch(function(error) {
         logger.error('Error during data seeding', error.stack);
+      });
+  };
+
+  content.forEach(function(data) {
+
+    // assume there can only have 1 and only 1 root user
+    PortalUser.findOne({
+      username: data.user.username
+    }, function(err, user) {
+      if (err) {
+        throw err;
+      }
+
+      if (user) {
+        logger.info('`' + data.user.username + '` user already exists; not seeding');
+        return;
+      }
+
+      insertData(data);
     });
   });
 }
