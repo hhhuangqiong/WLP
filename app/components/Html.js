46fb1f35a72a14f9668b8e5499af38bebe737de5
Gilbert Wong <gilbertwong@maaii.com>
2015-06-11 20:25:41 +0800

    Fix jquery file name inconsistency issue
    It's referenced in the "Html" react component.
    
    Here's how this is done:
    
    ```
    git rm -r public/vendor/jQuery
    bower install jquery --save
    ```
    
    Others:
    
    - add info for 'tag' & 'push' docker image
    - remove unused assets installed via Bower
    
    Jira: WLP-73
    
    Change-Id: I85d23b5ae7c4114643e52da2e8fbdf82c9b022ce

--- app/components/Html.js
+++ app/components/Html.js
@@ -22,28 +22,26 @@ var Html = React.createClass({
 
   render: function() {
     return (
-      <html>
+      <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge"  />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <title>{this.props.title}</title>
         <link href="http://fonts.googleapis.com/css?family=Roboto:500,300,700,400" rel="stylesheet" type="text/css" />
         <link rel="stylesheet" href="/stylesheets/iconic-fonts.css" />
         <link rel="stylesheet" href="/stylesheets/main.css" />
         <link rel="stylesheet" href="/vendor/react-date-picker/dist/react-datepicker.min.css" />
-        <link rel="stylesheet" href="/vendor/jquery-ui/themes/base/jquery-ui.css" />
         <link rel="stylesheet" href="/stylesheets/map-sprite.css" />
+        <script src="/vendor/modernizr/modernizr.js"></script>
       </head>
       <body>
         <div id="app" dangerouslySetInnerHTML={{__html: this.props.markup}}></div>
         <Version />
       </body>
       <script dangerouslySetInnerHTML={{__html: this.props.state}}></script>
-      <script src="/vendor/jquery/dist/jquery.js"></script>
-      <script src="/vendor/modernizr/modernizr.js"></script>
-      <script src="/vendor/foundation/js/foundation.js"></script>
-      <script src="/vendor/jquery-ui/ui/datepicker.js"></script>
+      <script src="/vendor/jquery/dist/jquery.min.js"></script>
+      <script src="/vendor/foundation/js/foundation.min.js"></script>
       <script src={bundlePath} defer></script>
       </html>
     );
