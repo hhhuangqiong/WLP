7b7c2d09fdc61a63dcd6c33c4935b9d4b381a765
Marston Ng <marstonng@maaii.com>
2015-07-16 11:40:10 +0800

    Added FilterBar Component
    tend to avoid leaving large html code block in any react component,
    also promote a more consistent and secure usage across components
    
    Change-Id: I22282daf20aead4125e117b3c1b3ec8ce98af91b

---
+++ app/main/components/FilterBar.js
@@ -0,0 +1,63 @@
+import _ from 'lodash';
+import React from 'react';
+
+let FilterBar = React.createClass({
+  render: function() {
+    return (
+      <nav className="top-bar top-bar--inner" data-topbar role="navigation">
+        <section className="top-bar-section">
+          {this.props.children}
+        </section>
+      </nav>
+    );
+  }
+});
+
+let FilterBarNavigationItems = React.createClass({
+  render: function() {
+    return (
+      <ul className="left top-bar--inner tab--inverted">
+        {React.Children.map(this.props.children, (child)=>{
+          return (
+            <li className="top-bar--inner tab--inverted__title">{child}</li>
+          );
+        })}
+      </ul>
+    )
+  }
+});
+
+let FilterBarLeftItems = React.createClass({
+  render: function() {
+    return (
+      <ul className="left">
+        {React.Children.map(this.props.children, (child)=>{
+          return (
+            <li>{child}</li>
+          );
+        })}
+      </ul>
+    )
+  }
+});
+
+let FilterBarRightItems = React.createClass({
+  render: function() {
+    return (
+      <ul className="right">
+        {React.Children.map(this.props.children, (child)=>{
+          return (
+            <li>{child}</li>
+          );
+        })}
+      </ul>
+    )
+  }
+});
+
+export {
+  FilterBar as Wrapper,
+  FilterBarLeftItems as LeftItems,
+  FilterBarRightItems as RightItems,
+  FilterBarNavigationItems as NavigationItems
+};
