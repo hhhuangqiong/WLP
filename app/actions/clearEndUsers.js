2fe57814bc3ac5a590b080bd5fb136ca06ec0e1b
Marston Ng <marstonng@maaii.com>
2015-07-23 19:04:24 +0800

    Fixed filename issue and deleted unused files
    Change-Id: I4b767045544eb37556ed40238c4615d41f088103
    Jira: WLP-55

--- app/actions/clearEndUsers.js
+++
@@ -1,9 +0,0 @@
-var debug = require('debug')('app:fetchEndUsers');
-
-export default function(context, { routeName, params, query }, done) {
-  context.dispatch('CLEAR_END_USERS_SUCCESS');
-
-  context.getRouter().transitionTo(routeName, params, _.omit(query, function(value) {
-    return !value;
-  }));
-};
