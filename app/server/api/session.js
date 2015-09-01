3815ae36178b25b9a115e96b6d7940c42cd347bb
Gilbert Wong <gilbertwong@maaii.com>
2015-09-01 18:51:42 +0800

    Fix 'cannot refresh' post-login issue
    Restore the accidentally replaced handler for the corresponding
    superagent API call.
    
    Change-Id: Id119623a7553538374eb1a33760558722a5d80f6

--- app/server/api/session.js
+++ app/server/api/session.js
@@ -5,16 +5,22 @@ import * as saUtil from '../../utils/superagent';
 import { SESSION } from '../paths';
 
 let debug = require('debug')('app:server/api/session');
-let genericHandler = _.partial(saUtil.genericHandler, debug);
 
 export default function(apiPrefix = '') {
   return {
     getSession: function(token, cb) {
       superagent
-        .get(`${this._getHost()}${apiPrefix}${SESSION}`)
-        .accept('json')
-        .set('Authorization', token)
-        .end(genericHandler(cb));
+      .get(`${this._getHost()}${apiPrefix}${SESSION}`)
+      .accept('json')
+      .set('Authorization', token)
+      .end(function(err, res) {
+        if (err) {
+          debug('error', err);
+        }
+
+        token = res && res.ok ? token : null;
+        cb(err, token);
+      });
     }
   }
 }
