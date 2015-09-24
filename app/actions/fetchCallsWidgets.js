54bdf6eca3fdac11a57cb11fdb9c23ba12d3ba9d
George Jor <georgejor@maaii.com>
2015-09-24 12:34:49 +0800

    Fix calls widget cannot be displayed problem
    Change-Id: I2ffd9e134a996f25579370df1ea8e45829d1025c

--- app/actions/fetchCallsWidgets.js
+++ app/actions/fetchCallsWidgets.js
@@ -1,3 +1,2 @@
 import actionCreator from '../main/utils/apiActionCreator';
-export default actionCreator('FETCH_CALLS_WIDGET', 'getCallsWidgets');
-
+export default actionCreator('FETCH_CALLS_WIDGETS', 'getCallsWidgets');
