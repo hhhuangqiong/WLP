0456dac8858b4bc5b06d7e203fbd09c582a64e89
Marco Fan <marcofan@maaii.com>
2015-10-13 10:46:19 +0800

    Change the "404 - not found" message in EndUserProfile's wallet section.
    - Also refactor the render method to make the DOM cleaner.
    
    Change-Id: I2aeee90afe9876d247d10fae98fd5de163d755fa
    JIRA: WLP-383

--- app/modules/end-user/components/EndUserProfile.js
+++ app/modules/end-user/components/EndUserProfile.js
@@ -86,7 +86,7 @@ var EndUserProfile = React.createClass({
         <div className="error text-center">
           <div className="error-description full-width">
             <i className="error-icon icon-error3" />
-            <span className="error-message">404 - not found!</span>
+            <span className="error-message">Wallet info unavailable</span>
           </div>
           <div className="error-button" onClick={this.handleRefreshButtonClick}>
             <i className="icon-refresh" />
@@ -96,7 +96,6 @@ var EndUserProfile = React.createClass({
     );
 
     if (this.props.user.wallets && this.props.user.wallets.length > 0) {
-
       // create an overview wallet
       let overviewWallet = {
         walletType: 'overview',
@@ -105,7 +104,7 @@ var EndUserProfile = React.createClass({
         balance: 0
       };
 
-      this.props.user.wallets.map((wallet)=>{
+      this.props.user.wallets.map((wallet) => {
         overviewWallet.balance += +wallet.balance;
         // the business logic saying that the expiry date
         // would always be the same, pick the latter one
@@ -118,7 +117,7 @@ var EndUserProfile = React.createClass({
       wallets = (
         <Accordion.Navigation title="Wallet Info">
           <WalletInfoItem wallet={overviewWallet} />
-          {this.props.user.wallets.map((wallet)=>{
+          {this.props.user.wallets.map((wallet) => {
             return (
               <WalletInfoItem wallet={wallet} />
             );
@@ -130,12 +129,70 @@ var EndUserProfile = React.createClass({
     return wallets;
   },
 
-  render: function() {
+  renderAccountPanel: function() {
     let country = _.find(Countries, (c) => {
       return c.alpha2.toLowerCase() === this.props.user.userDetails.countryCode;
     });
+
     let creationDate = moment(this.props.user.userDetails.creationDate).format(DATE_FORMAT);
 
+    return (
+      <Accordion.Navigation title="Account Info" hasIndicator={true} verified={this.props.user.userDetails.verified}>
+        <Item label="Created Time">{creationDate}</Item>
+        <Item label="Verified" capitalize={true}>
+          <If condition={this.props.user.userDetails.verified}>
+            <span className="verified">verified</span>
+            <Else />
+            <span className="unverified">unverified</span>
+          </If>
+        </Item>
+        <Item label="Country">
+          <div className="country-label">
+            <div className="flag__container left">
+              <span className={classNames('flag--' + country.alpha2, 'left')}/>
+            </div>
+            {country.name}
+          </div>
+        </Item>
+        <Item label="Mobile Number">{this.props.user.userDetails.username}</Item>
+        <Item label="Email">{this.props.user.userDetails.email || EMPTY_STRING}</Item>
+        <Item label="Pin">{this.props.user.userDetails.pin || EMPTY_STRING}</Item>
+        <Item label="Date of Birth">{this.props.user.userDetails.birthDate || EMPTY_STRING}</Item>
+        <Item label="Gender" capitalize={true}>
+          <span className="gender-label">
+            <i
+              className={classNames({'icon-male': this.props.user.userDetails.gender === 'male', 'icon-female': this.props.user.userDetails.gender === 'female'})}/>
+            {this.props.user.userDetails.gender || EMPTY_STRING}
+          </span>
+        </Item>
+      </Accordion.Navigation>
+    );
+  },
+
+  renderDevicePanel: function() {
+    return this.props.user.userDetails.devices.map((device) => {
+      return (
+        <Accordion.Navigation title="App Info">
+          <Item label="Device">
+            <span className="device-label">
+              <i className={classNames({'icon-apple': device.platform.toLowerCase() === 'ios'}, {'icon-android': device.platform.toLowerCase() === 'android'})} />
+              {device.platform}
+            </span>
+          </Item>
+          <Item label="Version">
+            <If condition={device.appVersionNumber}>
+              <span>v{device.appVersionNumber}</span>
+            <Else />
+              <span>{EMPTY_STRING}</span>
+            </If>
+          </Item>
+          <Item label="Language">{device.appLanguage}</Item>
+        </Accordion.Navigation>
+      );
+    });
+  },
+
+  render: function() {
     return (
       <If condition={this.props.user && this.props.user.userDetails}>
         <Panel.Wrapper addOn={true}>
@@ -151,56 +208,13 @@ var EndUserProfile = React.createClass({
               <p>{this.props.user.userDetails.displayName} ({this.props.user.userDetails.username})</p>
               <p>This operation cannot be undone. Delete anyway?</p>
             </ConfirmationDialog>
+
             <Accordion.Wrapper offsetMargin={true}>
               {this.renderWalletPanel()}
-              <Accordion.Navigation title="Account Info" hasIndicator={true} verified={this.props.user.userDetails.verified}>
-                <Item label="Created Time">{creationDate}</Item>
-                <Item label="Verified" capitalize={true}>
-                  <If condition={this.props.user.userDetails.verified}>
-                    <span className="verified">verified</span>
-                    <Else />
-                    <span className="unverified">unverified</span>
-                  </If>
-                </Item>
-                <Item label="Country">
-                  <div className="country-label">
-                    <div className="flag__container left">
-                      <span className={classNames('flag--' + country.alpha2, 'left')}/>
-                    </div>
-                    {country.name}
-                  </div>
-                </Item>
-                <Item label="Mobile Number">{this.props.user.userDetails.username}</Item>
-                <Item label="Email">{this.props.user.userDetails.email || EMPTY_STRING}</Item>
-                <Item label="Pin">{this.props.user.userDetails.pin || EMPTY_STRING}</Item>
-                <Item label="Date of Birth">{this.props.user.userDetails.birthDate || EMPTY_STRING}</Item>
-                <Item label="Gender" capitalize={true}>
-                  <span className="gender-label">
-                    <i
-                      className={classNames({'icon-male': this.props.user.userDetails.gender === 'male', 'icon-female': this.props.user.userDetails.gender === 'female'})}/>
-                    {this.props.user.userDetails.gender || EMPTY_STRING}
-                  </span>
-                </Item>
-              </Accordion.Navigation>
-              <For each="device" of={this.props.user.userDetails.devices}>
-                <Accordion.Navigation title="App Info">
-                  <Item label="Device">
-                    <span className="device-label">
-                      <i className={classNames({'icon-apple': device.platform.toLowerCase() === 'ios'}, {'icon-android': device.platform.toLowerCase() === 'android'})} />
-                      {device.platform}
-                    </span>
-                  </Item>
-                  <Item label="Version">
-                    <If condition={device.appVersionNumber}>
-                      <span>v{device.appVersionNumber}</span>
-                    <Else />
-                      <span>{EMPTY_STRING}</span>
-                    </If>
-                  </Item>
-                  <Item label="Language">{device.appLanguage}</Item>
-                </Accordion.Navigation>
-              </For>
+              {this.renderAccountPanel()}
+              {this.renderDevicePanel()}
             </Accordion.Wrapper>
+
             <If condition={this.props.user.userDetails.verified}>
               <div className="enduser-profile__control text-center">
                 <div className="enduser-profile__control__row">
