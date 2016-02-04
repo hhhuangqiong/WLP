71e7bdd65b642d6197111576690333b90e8b6b41
Marston Ng <marstonng@maaii.com>
2016-02-04 16:32:09 +0800

    Fixed too many wasted rendering
    Change-Id: I4d97d485b8fb748de8bdea88dcf7b06c0c697bfa

--- app/main/components/common/MainContent.js
+++ app/main/components/common/MainContent.js
@@ -2,6 +2,11 @@ import React from 'react';
 import {RouteHandler} from 'react-router';
 
 let MainContent = React.createClass({
+  shouldComponentUpdate() {
+    // confirmed safe to not update this component
+    return false;
+  },
+
   render: function() {
     return (
       <div className="row">
