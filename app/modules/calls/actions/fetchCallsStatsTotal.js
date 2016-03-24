fd2b727e90aafaf317e12589e26f56cd2fdf13f1
George Jor <georgejor@maaii.com>
2016-03-24 18:53:23 +0800

    Provide human readable error message for Call Stats
    Change-Id: I0068966ea4a18923df406bfda8fec14d6cd3fbc7
    Jira: WLP-700
    (cherry picked from commit 3f5f1de45cb82abffbc122e73614d1544d8b9916)

--- app/modules/calls/actions/fetchCallsStatsTotal.js
+++ app/modules/calls/actions/fetchCallsStatsTotal.js
@@ -1,14 +1,21 @@
 import actionCreator from '../../../main/utils/apiActionCreator';
 import { ERROR_MESSAGE } from '../../../main/constants/actionTypes';
 
+const debug = require('debug')('app:modules:calls:actions:fetchCallsStatsTotal');
+const CUSTOM_ERROR_MESSAGE = 'Please try again later, we are collecting the requested data.';
+
 export default actionCreator('FETCH_CALLS_STATS_TOTAL', 'getCallsStatsTotal', {
   cb: (err, response, context) => {
     context.dispatch('FETCH_CALLS_STATS_TOTAL_START');
 
     if (err) {
       context.dispatch('FETCH_CALLS_STATS_TOTAL_FAILURE', err);
+
+      // provide actual error message for debugging issue due to the use of custom error message
+      debug(err.message);
+
       context.dispatch(ERROR_MESSAGE, {
-        message: err.message,
+        message: CUSTOM_ERROR_MESSAGE,
       });
 
       return;
