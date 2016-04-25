ae01f3faa17c4f5ee9fd9853d347030dfda3d927
George Jor <georgejor@maaii.com>
2016-04-25 12:22:49 +0800

    Fix incorrect interoperability of using export default with babel 6
    Because babel6 needs explicit export when using destruct for latest ES6 standard
    
    Change-Id: Ib591104c1ef42c2a9ffc947bf595d0d9450bf86a
    Jira: WLP-694

--- app/utils/StringFormatter.js
+++ app/utils/StringFormatter.js
@@ -12,114 +12,111 @@ const NO_DURATIONS = '0s';
 const DECIMAL_PLACE = 1;
 const PRECISION = 3;
 
-export default {
-  /**
-   * @method parseDecimalData
-   * to give data accuracy to duration data in millisecond.
-   * when data > 10 milliseconds, it will be in significant figures,
-   * otherwise, it will give data in decimal places
-   *
-   * @param {integer} data - data in millisecond
-   * return {integer}
-   */
-  normalizeDurationInMS(data) {
-    // if data > 10ms (0.01s)
-    // use decimal place
-    if (data > 10) {
-      return data.toFixed(DECIMAL_PLACE);
+/**
+ * @method parseDecimalData
+ * to give data accuracy to duration data in millisecond.
+ * when data > 10 milliseconds, it will be in significant figures,
+ * otherwise, it will give data in decimal places
+ *
+ * @param {integer} data - data in millisecond
+ * return {integer}
+ */
+export function normalizeDurationInMS(data) {
+  // if data > 10ms (0.01s)
+  // use decimal place
+  if (data > 10) {
+    return data.toFixed(DECIMAL_PLACE);
+  }
+
+  // otherwise, use significant figures
+  return data.toPrecision(PRECISION);
+}
+
+/**
+ * @method parseDuration
+ * return millisecond to human readable time format
+ * @param {integer} duration - millisecond.
+ * @param {string} defaultLabel - display when duration is 0s.
+ */
+export function parseDuration(duration, defaultLabel = NO_DURATIONS) {
+  function getDuration(time, label) {
+    if (time >= 1) return `${time}${label} `;
+    return '';
+  }
+
+  // duration in millisecond, divided by 1000 to get seconds, apply ceiling to round up,
+  // times 1000 to transform to milliseconds again for process
+  const momentDuration = moment.duration(Math.ceil(duration / 1000) * 1000);
+
+  const hours = momentDuration.hours();
+  const minutes = momentDuration.minutes();
+  const seconds = momentDuration.seconds();
+
+  const durationString = `${getDuration(hours, HOUR_LABEL)}${getDuration(minutes, MINUTE_LABEL)}${getDuration(seconds, SECOND_LABEL)}`.trim();
+
+  if (!durationString.length) return defaultLabel;
+
+  return durationString;
+}
+
+export function getCountryName(countryAlpha2) {
+  if (!countryAlpha2) {
+    return null;
+  }
+
+  const country = CountryData.countries[(countryAlpha2 || '').toUpperCase()];
+  if (!country) return PLACEHOLDER_FOR_NULL;
+
+  const name = country.name;
+
+  // Country names containing ',' always have a reverse order of display:
+  // Micronesia, Federated States Of
+  // Iran, Islamic Republic Of
+  if (name.indexOf(',') > -1) {
+    const nameParts = name.split(',');
+    return `${nameParts[1]} ${nameParts[0]}`;
+  }
+
+  return name;
+}
+
+
+export function beautifyTime(timestamp, timeformat = OUTPUT_TIME_FORMAT) {
+  return timestamp ? moment(timestamp).format(timeformat) : PLACEHOLDER_FOR_NULL;
+}
+
+export function stringifyNumbers(number) {
+  return `'${number}'`;
+}
+
+/**
+ * Substract time and result it with specific format.
+ * @param {Object/string} time - Parsable string time or JS date object.
+ * @param {string} range -
+ *   A string representing time frame (e.g. '30 days', '24 hours')
+ *   that will be parsed by moment's manipulation.
+ * @param {string} format - Moment display format.
+ */
+export function subtractTime(time, range, format) {
+  const rangeValue = range.split(' ')[0];
+  const rangeFormat = range.split(' ')[1];
+
+  return moment(time).subtract(rangeValue, rangeFormat).format(format);
+}
+
+export function timeFromNow(range) {
+  const rangeValue = range.split(' ')[0];
+  const rangeFormat = range.split(' ')[1];
+
+  return moment().subtract(rangeValue, rangeFormat);
+}
+
+export function sanitizeNull(row, label = PLACEHOLDER_FOR_NULL) {
+  for (const exportField in row) {
+    if (!row[exportField]) {
+      row[exportField] = label;
     }
+  }
 
-    // otherwise, use significant figures
-    return data.toPrecision(PRECISION);
-  },
-
-  /**
-   * @method parseDuration
-   * return millisecond to human readable time format
-   * @param {integer} duration - millisecond.
-   * @param {string} defaultLabel - display when duration is 0s.
-   */
-  parseDuration(duration, defaultLabel = NO_DURATIONS) {
-    function getDuration(time, label) {
-      if (time >= 1) return `${time}${label} `;
-      return '';
-    }
-
-    // duration in millisecond, divided by 1000 to get seconds, apply ceiling to round up,
-    // times 1000 to transform to milliseconds again for process
-    const momentDuration = moment.duration(Math.ceil(duration / 1000) * 1000);
-
-    const hours = momentDuration.hours();
-    const minutes = momentDuration.minutes();
-    const seconds = momentDuration.seconds();
-
-    const durationString = `${getDuration(hours, HOUR_LABEL)}${getDuration(minutes, MINUTE_LABEL)}${getDuration(seconds, SECOND_LABEL)}`.trim();
-
-    if (!durationString.length) return defaultLabel;
-
-    return durationString;
-  },
-
-  getCountryName(countryAlpha2) {
-    if (!countryAlpha2) {
-      return null;
-    }
-
-    const country = CountryData.countries[(countryAlpha2 || '').toUpperCase()];
-    if (!country) return PLACEHOLDER_FOR_NULL;
-
-    const name = country.name;
-
-    // Country names containing ',' always have a reverse order of display:
-    // Micronesia, Federated States Of
-    // Iran, Islamic Republic Of
-    if (name.indexOf(',') > -1) {
-      const nameParts = name.split(',');
-      return `${nameParts[1]} ${nameParts[0]}`;
-    }
-
-    return name;
-  },
-
-
-  beautifyTime(timestamp, timeformat = OUTPUT_TIME_FORMAT) {
-    return timestamp ? moment(timestamp).format(timeformat) : PLACEHOLDER_FOR_NULL;
-  },
-
-
-  stringifyNumbers(number) {
-    return `'${number}'`;
-  },
-
-  /**
-   * Substract time and result it with specific format.
-   * @param {Object/string} time - Parsable string time or JS date object.
-   * @param {string} range -
-   *   A string representing time frame (e.g. '30 days', '24 hours')
-   *   that will be parsed by moment's manipulation.
-   * @param {string} format - Moment display format.
-   */
-  subtractTime(time, range, format) {
-    const rangeValue = range.split(' ')[0];
-    const rangeFormat = range.split(' ')[1];
-
-    return moment(time).subtract(rangeValue, rangeFormat).format(format);
-  },
-
-  timeFromNow(range) {
-    const rangeValue = range.split(' ')[0];
-    const rangeFormat = range.split(' ')[1];
-
-    return moment().subtract(rangeValue, rangeFormat);
-  },
-
-  sanitizeNull(row, label = PLACEHOLDER_FOR_NULL) {
-    for (const exportField in row) {
-      if (!row[exportField]) {
-        row[exportField] = label;
-      }
-    }
-
-    return row;
-  },
-};
+  return row;
+}
--- test/unit/app/server/utils/stringFormatter.js
+++ test/unit/app/server/utils/stringFormatter.js
@@ -1,5 +1,5 @@
-var expect = require('chai').expect;
-var StringFormatter = require('app/utils/StringFormatter').default;
+import { expect } from 'chai';
+import { parseDuration } from 'app/utils/StringFormatter';
 
 /**
  * Design for a more readable test case
@@ -7,19 +7,16 @@ var StringFormatter = require('app/utils/StringFormatter').default;
  * @param second - numebr of seconds
  */
 function toMilliseconds(second) {
-  return second*1000;
+  return second * 1000;
 }
 
-describe('To test different format parser', function() {
-  var parseDuration = StringFormatter.parseDuration;
-
-  describe('#durationForHuman', function() {
-
-    it('should return 0s if there is no duration', function() {
+describe('To test different format parser', () => {
+  describe('#durationForHuman', () => {
+    it('should return 0s if there is no duration', () => {
       expect(parseDuration(0)).to.equal('0s');
     });
 
-    it('should return 1s if the millisecond is less than 1000', function() {
+    it('should return 1s if the millisecond is less than 1000', () => {
       expect(parseDuration(1)).to.equal('1s');
       expect(parseDuration(500)).to.equal('1s');
       expect(parseDuration(999)).to.equal('1s');
@@ -27,21 +24,21 @@ describe('To test different format parser', function() {
       expect(parseDuration(1000)).to.not.equal('0s');
     });
 
-    it('should return 1s', function() {
+    it('should return 1s', () => {
       expect(parseDuration(1000)).to.equal('1s');
       expect(parseDuration(1001)).to.equal('2s');
       expect(parseDuration(1999)).to.equal('2s');
       expect(parseDuration(2000)).to.equal('2s');
     });
 
-    it('should return in seconds', function() {
+    it('should return in seconds', () => {
       expect(parseDuration(toMilliseconds(59))).to.equal('59s');
 
       expect(parseDuration(toMilliseconds(60))).to.not.equal('60s');
       expect(parseDuration(toMilliseconds(61))).to.not.equal('61s');
     });
 
-    it('should return in minutes', function() {
+    it('should return in minutes', () => {
       expect(parseDuration(toMilliseconds(60))).to.equal('1m');
       expect(parseDuration(toMilliseconds(120))).to.equal('2m');
 
@@ -49,7 +46,7 @@ describe('To test different format parser', function() {
       expect(parseDuration(toMilliseconds(6000))).to.not.equal('100m');
     });
 
-    it('should return in minutes with seconds', function() {
+    it('should return in minutes with seconds', () => {
       expect(parseDuration(toMilliseconds(61))).to.equal('1m 1s');
       expect(parseDuration(toMilliseconds(62))).to.equal('1m 2s');
       expect(parseDuration(toMilliseconds(121))).to.equal('2m 1s');
@@ -61,20 +58,20 @@ describe('To test different format parser', function() {
       expect(parseDuration(toMilliseconds(3601))).to.not.equal('1h');
     });
 
-    it('should return in hours', function() {
+    it('should return in hours', () => {
       expect(parseDuration(toMilliseconds(3600))).to.equal('1h');
       expect(parseDuration(toMilliseconds(7200))).to.equal('2h');
     });
 
-    it('should return in hours with seconds', function() {
+    it('should return in hours with seconds', () => {
       expect(parseDuration(toMilliseconds(3601))).to.equal('1h 1s');
     });
 
-    it('should return in hours with mintues', function() {
+    it('should return in hours with mintues', () => {
       expect(parseDuration(toMilliseconds(3660))).to.equal('1h 1m');
     });
 
-    it('should return in hours with mintues and/or seconds', function() {
+    it('should return in hours with mintues and/or seconds', () => {
       expect(parseDuration(toMilliseconds(3659))).to.not.contain('m');
       expect(parseDuration(toMilliseconds(3661))).to.equal('1h 1m 1s');
     });
