5d8bf70287100076dd57370e2ccaa14af9367d38
George Jor <georgejor@maaii.com>
2016-05-11 18:09:44 +0800

    More VSF action types to constants for better control
    Change-Id: I6498bcb7839c1a1d172864f8222db8035e561753
    Jira: WLP-791

--- app/modules/vsf/actions/stats.js
+++ app/modules/vsf/actions/stats.js
@@ -1,44 +1,34 @@
-import { ERROR_MESSAGE } from '../../../main/constants/actionTypes';
 import actionCreator from '../../../main/utils/apiActionCreator';
 
+import {
+  CLEAR_VSF_MONTHLY_STATS,
+  CLEAR_VSF_SUMMARY_STATS,
+  UPDATE_VSF_MONTHLY_STATS_DATE,
+  UPDATE_VSF_SUMMARY_STATS_TIME_FRAME,
+  FETCH_VSF_MONTHLY_STATS,
+  FETCH_VSF_SUMMARY_STATS,
+} from '../constants/actionTypes';
+
 export function clearVsfMonthlyStats(context, params, done) {
-  context.dispatch('CLEAR_VSF_MONTHLY_STATS');
+  context.dispatch(CLEAR_VSF_MONTHLY_STATS);
   done();
 }
 
 export function clearVsfSummaryStats(context, params, done) {
-  context.dispatch('CLEAR_VSF_SUMMARY_STATS');
+  context.dispatch(CLEAR_VSF_SUMMARY_STATS);
   done();
 }
 
 export function updateVsfMonthlyStatsDate(context, params, done) {
-  context.dispatch('UPDATE_VSF_MONTHLY_STATS_DATE', params.date);
+  context.dispatch(UPDATE_VSF_MONTHLY_STATS_DATE, params.date);
   done();
 }
 
 export function updateVsfSummaryStatsTimeFrame(context, params, done) {
-  context.dispatch('UPDATE_VSF_SUMMARY_STATS_TIME_FRAME', params.timeFrame);
+  context.dispatch(UPDATE_VSF_SUMMARY_STATS_TIME_FRAME, params.timeFrame);
   done();
 }
 
-export const fetchVsfMonthlyStats = actionCreator('FETCH_VSF_MONTHLY_STATS', 'getVsfMonthlyStats', {
-  cb: (error, response, context, params) => {
-    if (error) {
-      context.dispatch(ERROR_MESSAGE, error);
-      return;
-    }
+export const fetchVsfMonthlyStats = actionCreator(FETCH_VSF_MONTHLY_STATS, 'getVsfMonthlyStats');
 
-    context.dispatch('FETCH_VSF_MONTHLY_STATS_SUCCESS', response);
-  },
-});
-
-export const fetchVsfSummaryStats = actionCreator('FETCH_VSF_SUMMARY_STATS', 'getVsfMonthlyStats', {
-  cb: (error, response, context, params) => {
-    if (error) {
-      context.dispatch(ERROR_MESSAGE, error);
-      return;
-    }
-
-    context.dispatch('FETCH_VSF_SUMMARY_STATS_SUCCESS', response);
-  },
-});
+export const fetchVsfSummaryStats = actionCreator(FETCH_VSF_SUMMARY_STATS, 'getVsfSummaryStats');
