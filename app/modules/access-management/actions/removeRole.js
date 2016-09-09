e4404826674626050ae32ddd7a01446eb3848914
Andy Hui <AndyHui@maaii.com>
2016-09-09 12:26:38 +0800

    fix: error message not prompt when delete Admin role
    Change-Id: I0690a82fa4edd7bdb3bbbf10a3dcf9270d413b68

--- app/modules/access-management/actions/removeRole.js
+++ app/modules/access-management/actions/removeRole.js
@@ -1,10 +1,2 @@
-export default function (context, params) {
-  context.dispatch('REMOVE_ROLE_START');
-  context.api.deleteRole(params, (err) => {
-    if (err) {
-      context.dispatch('REMOVE_ROLE_FAILURE', err);
-      return;
-    }
-    context.dispatch('REMOVE_ROLE_SUCCESS', params);
-  });
-}
+import actionCreator from '../../../main/utils/apiActionCreator';
+export default actionCreator('REMOVE_ROLE', 'deleteRole');
